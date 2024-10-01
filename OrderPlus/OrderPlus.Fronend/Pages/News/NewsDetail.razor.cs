24e32a35dd040598f96e6e72a3dae4a830333daa
AhmedJwad <Ahmednet380@gmail.com>
2024-10-01 08:43:52 -0800

    Add news articles functionality with image support

---
+++ OrderPlus/OrderPlus.Fronend/Pages/News/NewsDetail.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using OrderPlus.Fronend.Repositories;
+using OrderPlus.Shared.Entites;
+using System.Net;
+
+namespace OrderPlus.Fronend.Pages.News
+{
+    public partial class NewsDetail
+    {
+        private NewsArticle? newsArticle;
+        private bool loading = true;
+
+        [Inject] private IRepository repository { get; set; } = null!;
+        [Inject] private NavigationManager navigationManager { get; set; } = null!;
+        [Inject] private ISnackbar snackbar { get; set; } = null!;
+
+        [EditorRequired, Parameter] public int Id { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            loading = true;
+            var responseHttp = await repository.GetAsync<NewsArticle>($"/api/news/{Id}");
+            loading = false;
+
+            if (responseHttp.Error)
+            {
+                if (responseHttp.HttpResponseMessage.StatusCode == HttpStatusCode.NotFound)
+                {
+                    navigationManager.NavigateTo("/news");
+                }
+                else
+                {
+                    var messsage = await responseHttp.GetErrorMessageAsync();
+                    snackbar.Add(messsage, Severity.Error);
+                }
+            }
+            else
+            {
+                newsArticle = responseHttp.Response;
+            }
+        }
+
+    }
+}
---
+++ OrderPlus/OrderPlus.Fronend/Pages/NewsAndPromotions.razor.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using OrderPlus.Fronend.Pages.News;
+using OrderPlus.Fronend.Repositories;
+using OrderPlus.Shared.Entites;
+
+namespace OrderPlus.Fronend.Pages
+{
+    public partial class NewsAndPromotions
+    {
+        private List<NewsArticle>? newsArticles;
+        private const string baseUrl = "api/news";
+        private bool loading = true;
+
+        [Inject] private IRepository repository { get; set; } = null!;
+        [Inject] private ISnackbar snackbar { get; set; } = null!;
+        [Inject] private IDialogService dialogService { get; set; } = null!;
+
+        protected override async Task OnInitializedAsync()
+        {
+           await LoadAsync();
+        }
+
+        private async Task LoadAsync()
+        {
+            loading = true;
+            var url = $"{baseUrl}?page=1&recordsnumber={int.MaxValue}&Id=1";
+            var responseHttp = await repository.GetAsync<List<NewsArticle>>(url);
+            loading = false;
+
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                snackbar.Add(message, Severity.Error);
+            }
+
+            newsArticles = responseHttp.Response;
+        }
+        private static string TruncateContent(string content, int length)
+        {
+            if (content.Length > length)
+            {
+                return content.Substring(0, length) + "...";
+            }
+            return content;
+        }
+
+        private void ViewDetails(NewsArticle news)
+        {
+            var options = new DialogOptions() { CloseOnEscapeKey = true, CloseButton = true };
+            var parameters = new DialogParameters
+            {
+                { "Id", news.Id }
+            };
+            dialogService.Show<NewsDetail>(news.Title, parameters, options);
+        }
+    }
+}
